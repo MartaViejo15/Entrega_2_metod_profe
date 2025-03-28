1c11b676e03454e1112b0e0f1d29cb975ac89244
Martu <martavgarcia15@hotmail.com>
2025-03-28 11:47:42 +0100

    commit

--- src/Ejercicio_1/ScannerFromFile.java
+++ src/Ejercicio_1/ScannerFromFile.java
@@ -15,8 +15,17 @@ public class ScannerFromFile {
             Scanner in = new Scanner(new File("test.in"));
         }catch (FileNotFoundException e){
             System.out.println(e);
-        }
-        /* Esto lo que hace es escribir la excepcion en la terminal sin que de el error
+            /* Esto lo que hace es escribir la excepción en la terminal sin que dé el error
            en rojo */
+        } finally {
+        System.out.println("Scanner finished");
+        }
+
+        /* Al usar un try-catch le pedimos que pruebe lo del try y en caso de dar error
+        que imprima por pantalla el FileNotFoundException, que es lo que dice el catch,
+        y al añadirle un finally le decimos que siempre imprima el string "Scanner finished",
+        se dé o no la excepción.
+         */
+
     }
 }
