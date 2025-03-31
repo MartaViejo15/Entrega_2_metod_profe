dfd8b6ded0d02839e82eb056d3bbfa671d465a86
Martu <martavgarcia15@hotmail.com>
2025-03-31 21:15:18 +0200

    commit

--- src/Ejercicio_1/ScannerFromFileWithCatch.java
+++ src/Ejercicio_1/ScannerFromFileWithCatch.java
@@ -8,13 +8,13 @@ public class ScannerFromFileWithCatch {
     public static void main(String[] args) {
         try {
             Scanner in = new Scanner(new File("test.in"));
-// do something if no exception ...
-// you main logic here in the try-block
+            // do something if no exception ...
+            // you main logic here in the try-block
         } catch (FileNotFoundException ex) { // error handling separated from the main logic
             ex.printStackTrace(); // print the stack trace
         }
-        /* El printStackTrace lo que hace es imprimir la pila de llamadas al metodo para
-           poder saber como se ha depurado el código e ir realizando operaciones de limpieza */
+        /* El printStackTrace() lo que hace es imprimir el rastreo de pila, la pila de llamadas al metodo,
+         para poder saber cómo se ha depurado el código e ir realizando operaciones de limpieza */
     }
 }
 
